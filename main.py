b87bc716d3246d7848a477607d857c8905ae8284
RahulM-3 <rahulcode97@gmail.com>
2024-03-03 00:19:38 +0530

    reverting to f5de00d

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from os import getenv
 from typing import Final
 from telegram import Bot
+from telegram import Bot
 from telegram import Update
 from telegram.ext import Application , CommandHandler , MessageHandler , filters ,ContextTypes
 
@@ -10,14 +11,19 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TOKEN:Final = getenv("apiToken")
-
 telebot = Bot(TOKEN)
 
 async def imagine_command(update:Update, context: ContextTypes.DEFAULT_TYPE):
     userprompt = update.message.text[9:]
     print(userprompt)
     await update.message.reply_text('under_development')
+    botreply = await telebot.send_message(chat_id=update.message.chat_id, text='loading.  ', reply_to_message_id=update.message.id)
 
+    sleep(1)
+    await telebot.edit_message_text(text="loading.. ", message_id=botreply.id, chat_id=botreply.chat_id)
+
+    sleep(1)
+    await telebot.edit_message_text(text="loading...", message_id=botreply.id, chat_id=botreply.chat_id)
 
 async def help_command(update:Update,  context:ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("""
@@ -26,7 +32,6 @@ async def help_command(update:Update,  context:ContextTypes.DEFAULT_TYPE):
         /help
         /imagine [prompt] (under development)   
                                     """)
-    
 
 
 if __name__=='__main__':
