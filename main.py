f5de00de9907aa62cb6666605f002a916604ef96
RahulM-3 <rahulcode97@gmail.com>
2024-03-02 21:28:20 +0530

    message edit for loading display

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 from os import getenv
 from typing import Final
+from telegram import Bot
+from telegram import Bot
 from telegram import Update
 from telegram.ext import Application , CommandHandler , MessageHandler , filters ,ContextTypes
 
+from time import sleep # temp
+
 TOKEN:Final = getenv("apiToken")
-   
-   
+
+telebot = Bot(TOKEN)
+
 async def imagine_command(update:Update, context: ContextTypes.DEFAULT_TYPE):
     userprompt = update.message.text[9:]
-    print(userprompt)
-    await update.message.reply_text('under_development')
+    botreply = await telebot.send_message(chat_id=update.message.chat_id, text='loading.  ', reply_to_message_id=update.message.id)
+    
+    sleep(1)
+    await telebot.edit_message_text(text="loading.. ", message_id=botreply.id, chat_id=botreply.chat_id)
+
+    sleep(1)
+    await telebot.edit_message_text(text="loading...", message_id=botreply.id, chat_id=botreply.chat_id)
 
 async def help_command(update:Update,  context:ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("""
