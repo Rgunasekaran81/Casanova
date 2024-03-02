9237fd46f953c8ac46090d15206af472acb2c63d
rgunasekaran003 <133247797+Rgunasekaran81@users.noreply.github.com>
2024-03-02 20:09:48 +0530

    imagine commandadd

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from os import getenv
 from typing import Final
-import telegram
+from telegram import Update
 from telegram.ext import Application , CommandHandler , MessageHandler , filters ,ContextTypes
 
 TOKEN:Final = getenv("apiToken")
+   
+   
+async def imagine_command(update:Update,context: ContextTypes.DEFAULT_TYPE):
+ await update.message.reply_text('under_development')
 
+
+
+
+
+if __name__=='__main__':
+ app=Application.builder().token(TOKEN).build()
+
+ app.add_handler(CommandHandler('imagine',imagine_command))
+ app.run_polling(poll_interval=3)
