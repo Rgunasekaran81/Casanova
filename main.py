eef5f8ee0685c9aa301096bbe0bc9019ad5e0d1a
rgunasekaran003 <133247797+Rgunasekaran81@users.noreply.github.com>
2024-03-03 14:45:30 +0530

    handling diffrnt root comands

--- main.py
+++ main.py
@@ -15,20 +15,35 @@ TOKEN:Final = getenv("apiToken")
 telebot = Bot(TOKEN)
 
 async def root_command(update:Update, context: ContextTypes.DEFAULT_TYPE):
-    Username, password = update.message.text.replace("/start ", "").split(", ") #[rahul, 123]
-    #print(Username , password)
+    command = update.message.text.replace("/root ", "")
+    print(command)
+    if("login" in command):
+        if(len(command)==5):
+            await telebot.send_message(chat_id=update.message.chat_id, text='<username>, <password>\nEnter your user name and password in this format ', reply_to_message_id=update.message.id)
+        else:
+            pass
+        Username, password = update.message.text.replace("/root ", "").split(", ") #[rahul, 123]
+        print(Username , password)
 
-    data = {}
-    with open("database.json", "r") as database:
-        data = json.load(database)
-        data[Username] = [password]   
-    with open("database.json", "w") as database:
-        json.dump(data, database, indent = 4)
+        data = {}
+        with open("database.json", "r") as database:
+            data = json.load(database)
+            data[Username] = [password]   
+        with open("database.json", "w") as database:
+            json.dump(data, database, indent = 4)
  
+    elif(command == "register"):
+        pass
+    elif(command == "show prompt"):
+        pass
+    elif(command == "delete prompt"):
+        pass
+    elif(command == "delete user"):
+        pass
+   
 async def imagine_command(update:Update, context: ContextTypes.DEFAULT_TYPE):
     userprompt = update.message.text[9:]
     print(userprompt)
-    await update.message.reply_text('under_development')
     botreply = await telebot.send_message(chat_id=update.message.chat_id, text='loading.  ', reply_to_message_id=update.message.id)
 
     sleep(1)
